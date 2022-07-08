7eafe19934e09b8467bef3bf82f05fa59ca391c1
abdullah azzam <dfeqhasavrymchaplctionnphysics@gmail.com>
2022-07-08 13:32:53 +0800

    updated

--- lib/components/novelCard.dart
+++ lib/components/novelCard.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
 import '../network/DataProvider.dart';
 
 class NovelCard extends StatefulWidget {
